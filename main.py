999b7595af1490e9ce9a444a5637db423fd59ba7
Ahmad Mustafa <ahmadmutafa15@gmail.com>
2026-07-23 16:12:37 +0500

    added put and delete

--- main.py
+++ main.py
@@ -13,8 +13,20 @@ students = [
     Student(
         id=2,
         name="Abdul Hadi",
+        age=20,
+        course="Software Engineering"
+    ),
+        Student(
+        id=3,
+        name="Faizan Mudassir",
         age=21,
         course="Software Engineering"
+    ),
+        Student(
+        id=4,
+        name="Ahmed Furqan",
+        age=21,
+        course="Computer Science"
     )
 ]
 
@@ -30,7 +42,7 @@ def get_students():
 def get_student(student_id: int):
 
     for student in students:
-        if student["id"] == student_id:
+        if student.id == student_id:
             return student
 
     return {"message": "Student not found"}
@@ -38,9 +50,28 @@ def get_student(student_id: int):
 @app.post("/students")
 def add_student(student: Student):
 
-    students.append(student.dict())
+    students.append(student)
 
-    return {
-        "message": "Student added successfully",
-        "student": student
-    }
+    return { "message": "Student added successfully"}
+
+@app.put("/students")
+def update_student(id: int, name: str, age: int, course: str):
+    for student in students:
+        if student.id == id:
+            student.name = name
+            student.age = age
+            student.course = course
+
+            return {"message": "Student updated successfully"}
+
+    return {"message": "Student not found"}
+
+@app.delete("/students/{student_id}")
+def delete_student(student_id: int):
+    for student in students:
+        if student.id == student_id:
+            students.remove(student)
+            
+            return {"message": "Student deleted successfully"}
+
+    return {"message": "Student not found"}
